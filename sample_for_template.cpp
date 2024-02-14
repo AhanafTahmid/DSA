46b2869272bd9486e0f36428b2bcd4d18dc5c534
AhanafTahmid <ahanaftahmid55@gmail.com>
2024-02-14 23:16:25 +0600

    .

---
+++ sample_for_template.cpp
@@ -0,0 +1,51 @@
+
+
+---------------------------------------------------------------------------------------------------------
+Template - 1(Geekforgeeks or other website practicing)
+---------------------------------------------------------------------------------------------------------
+
+//#######-------Name--------########
+
+/*
+Input: 
+Output: 
+------
+
+My sample input:
+
+Output-> 
+
+
+
+//https://www.geeksforgeeks.org/given-two-sorted-arrays-number-x-find-pair-whose-sum-closest-x/
+
+Step-by-step approach:
+
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+
+
+void solve(){
+
+}
+
+
+signed main(){
+    int t=1;
+    //cin >> t;
+    while(t--)solve(); 
+    return 0;
+}
+
+
+
+
+---------------------------------------------------------------------------------------------------------
+Template - 2( - )
+---------------------------------------------------------------------------------------------------------
