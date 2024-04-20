67695e4419748c17359c6c23835ef02bd036c8bb
AhanafTahmid <ahanaftahmid55@gmail.com>
2024-04-21 00:30:24 +0600

    .

---
+++ bitwise/bitwise(priyansh).cpp
@@ -0,0 +1,68 @@
+// Resource: https://youtu.be/LGrE0siZ-ZA
+//bitwise Operators
+//1. even or odd
+    // x&1
+    cout<< (5&1) <<endl;//prints 1, odd
+    cout<< (6&1) <<endl;//prints 0, even
+
+//2. Power of two
+    // x & (x-1)
+    cout<< (32 & (32-1)) <<endl;//prints 0, cause 32 is power two
+    cout<< (5 & (5-1)) <<endl;//prints 4, non zero value
+
+//3. Manipulating bits
+    //check if kth bit is set
+    // x & (1<<k)
+    cout<< (4 & (1<<2)) <<endl;//prints 4, as 4th bit is set
+
+    //Toggle the kth bit
+    // x ^ (1<<k)
+    cout<< (4 ^ (1<<2)) <<endl;//prints 0, toggle the 4th bit
+    cout<< (4 ^ (1<<1)) <<endl;//prints 6, toggle the 2th bit, 100 ^ 010 -> 110
+
+    //Set the kth bit
+    // x | (1<<k)
+    cout<< (4 | (1<<1)) <<endl;//prints 6, sets the 2nd bit 1
+
+    //Unset the kth bit
+    // x ~ (1<<k)
+    cout<< (4 &  ~(1<<2)) <<endl;//prints 0
+    cout<< (4 &  ~(1<<1)) <<endl;//prints 4
+
+//4. Multiply or divide a number
+    //Multiply
+    // x << k
+    cout<< (4 << 1) <<endl;//8
+    cout<< (4 << 3) <<endl;//32 -> 4 * 8
+
+    //Division
+    // x >> k
+    cout<< (4 >> 1) <<endl;//0
+    cout<< (16 >> 1) <<endl;//8
+
+//5. Find x % 2^k
+    // x & ((1<<k)-1)
+    cout<< (4 << 1) <<endl;//8
+
+//6. Swap 2 numbers
+    // x = x^y
+    // y = x^y
+    // x = x^y
+    int t1 = 12; 
+    int t2 = 112;
+    cout<< t1 <<' '<<t2<<endl;//12 112
+    t1 = t1 ^ t2;
+    t2 = t1 ^ t2;
+    t1 = t1 ^ t2;
+    cout<< t1 <<' '<<t2<<endl;//112 12
+
+//7. Toggle assignment(Finding out the the number)
+    // x = a^b^x;
+
+//8. important identities
+    // a + b = (a^b) + 2 (a&b)
+    // a + b = (a|b) + (a&b)
+
+//9. Count set bits
+    cout<< __builtin_popcount(4) <<endl;// 1
+    cout<< __builtin_popcountll(4) <<endl;//1
---
+++ bitwise/practice (striver).cpp
@@ -0,0 +1,40 @@
+//#######-------Name--------########
+
+/*
+Input: 
+Output: 
+------
+
+My sample input:
+
+Output-> 
+
+
+
+//https://www.geeksforgeeks.org/given-two-sorted-arrays-number-x-find-pair-whose-sum-closest-x/
+
+Step-by-step approach:
+
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+
+
+void solve(){
+
+}
+
+
+signed main(){
+    int t=1;
+    //cin >> t;
+    while(t--)solve(); 
+    return 0;
+}
+
+
+
