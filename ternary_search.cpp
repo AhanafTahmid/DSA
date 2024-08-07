0146ed37a9672375670f94b8c8f748e02cc4d1be
AhanafTahmid <ahanaftahmid55@gmail.com>
2024-08-08 02:41:00 +0600

    .

---
+++ ternary_search.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+
+void solve(){
+    int l = 1, r = 1001, m;
+    for(int i=0;i<100;i++){
+        int m1 = l + (r - l) / 3;
+        int m2 = r - (r - l) / 3;
+        
+    }
+}
+
+int32_t main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int t=1;
+    cin >> t;
+    while(t--)solve(); 
+    return 0;
+}
