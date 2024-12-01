7c3f2f58e750c84a5ee6ba5522951db54e0e999b
AhanafTahmid <ahanaftahmid55@gmail.com>
2024-12-02 01:55:30 +0600

    .

--- CSES/Mathematics/number theory template/mod_inverse.cpp
+++ CSES/Mathematics/number theory template/mod_inverse.cpp
@@ -1,7 +1,7 @@
 //Main Inverse template, for divisional power
 int md = 1e9 + 7;
-ll power(ll a, ll b, ll m) {
-    ll res = 1;
+int power(int a, int b, int m) {
+    int res = 1;
     while (b > 0) {
         if (b & 1)
             res = (res * a)%m;
@@ -12,7 +12,7 @@ ll power(ll a, ll b, ll m) {
 }
 
 
-ll inverse(ll i){
+int inverse(int i){
     return power(i, md-2, md);//binexpo(tt,M-2,M); = a^(M-2)%M
 }
 
@@ -24,7 +24,7 @@ ll inverse(ll i){
 
 
 int md = 1e9 + 7;
-ll inverse(ll i){
+int inverse(int i){
     if(i==1) return 1;
     return (md - ((md/i)*inverse(md%i))%md+md)%md;
 }
--- CSES/Mathematics/number theory template/modular_exponentiation.cpp
+++ CSES/Mathematics/number theory template/modular_exponentiation.cpp
@@ -108,3 +108,20 @@ int power(int a,int b){
     }
     return ans;
 }
+
+
+
+
+//#################################
+
+int md = 1e9 + 7;
+int power(int a, int b, int m) {
+    int res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = (res * a)%m;
+        a = (a * a) %m;
+        b >>= 1;
+    }
+    return res;
+}
